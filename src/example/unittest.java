e4c52f21425032653945f92d692b02e6e8cd0f5a
randomguy <tmthien09@gmail.com>
2025-03-21 10:27:51 +0700

    n

---
+++ src/example/unittest.java
@@ -0,0 +1,17 @@
+package example;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.entities.Units.*;
+
+public class unittest extends UnitType {
+	
+	static void unittest(string nam) {
+		super();
+		health = armor = 5000000f;
+		type = fly;
+	}
+	static void unitDeath(int s)
+		UnitCreateEvent()
+}
